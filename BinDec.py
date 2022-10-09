0f2a2012b03f55734c6b8f7f49955756d454edda
AlastorCarmesi <carmesiblog7@gmail.com>
2022-10-08 21:56:50 -0500

    Decimal a Binarios

---
+++ BinDec.py
@@ -0,0 +1,9 @@
+def binario(decimal):
+    binario = ""
+    while decimal // 2 != 0:
+        binario = str(decimal % 2) + binario
+        decimal = decimal // 2
+    return str(decimal) + binario
+
+Num = int (input("introduce algun numero: "))
+print(binario(Num))
